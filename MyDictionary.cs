bc911dd41f2cfd01e7e9540e7a046bb4e84bf928
Abdullah Taha AKSAKAL <aaksakal050@gmail.com>
2021-10-31 16:50:10 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] _tempKey;
+        TValue[] _tempValue;
+        TKey[] _key;
+        TValue[] _value;
+
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempValue = _value;
+            _tempKey = _key;
+
+            _value = new TValue[_tempValue.Length + 1];
+            _key = new TKey[_tempKey.Length + 1];
+
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+
+        public int Length
+        {
+            get { return _key.Length; }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < _key.Length; i++)
+            {
+                Console.WriteLine("Key = " + _key[i] + " Value = " + _value[i]);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(1, "Abdullah Taha");
+            students.Add(2, "Mehmet");
+            students.Add(3, "Ahmet");
+            Console.WriteLine(students.Length);
+            students.Show();
+        }
+    }
+}
